4e5b481c3b21ef2069506212326093969c0206ab
Dean Fogarty <dfogarty@raywhite.com>
2023-07-12 11:29:53 +1000

    Cut-and-paste some tailwind-ui examples.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,60 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    const samples = [
+        {
+            tailwindReference: 'https://tailwindui.com/components/application-ui/application-shells/sidebar#component-5548358cb34897c6b28551f2ad885eec',
+            title: 'Dark sidebar with header',
+            route: '/dark-sidebar-with-header',
+        },
+        {
+            tailwindReference: 'https://tailwindcss.com/docs/plugins#forms',
+            title: 'Form components',
+            route: '/form-components',
+        },
+        {
+            tailwindReference: 'https://tailwindui.com/components/application-ui/navigation/steps',
+            title: 'Steppers',
+            route: '/steps'
+        },
+        {
+            tailwindReference: 'https://tailwindui.com/components/application-ui/overlays/slide-overs',
+            title: 'Slide overlays (basic)',
+            route: '/slide-overlay-basic'
+        },
+        {
+            tailwindReference: 'https://tailwindui.com/components/application-ui/overlays/slide-overs#component-b76ed25b74c40b7f3a181393e2b7742f',
+            title: 'Slide overlays (with form)',
+            route: '/slide-overlay-with-form'
+        },
+        {
+            tailwindReference: 'https://tailwindui.com/components/application-ui/layout/dividers',
+            title: 'Dividers',
+            route: '/dividers',
+        },
+    ];
+</script>
+
+<div class="m-4">
+    <div>
+        <hr class="mt-4 mb-4" />
+
+        <h1 class="text-xl">Samples</h1>
+        <ul class="list-disc m-4">
+            {#each samples as sample}
+                <li><a class="hover:underline" href={sample.route}>{sample.title}</a> (<a class="hover:underline" href={sample.tailwindReference}>tailwind docs</a>)</li>
+            {/each}
+        </ul>
+    </div>
+
+    <div>
+        <hr class="mt-4 mb-4" />
+
+        <h1 class="text-xl">Tailwind doc reference</h1>
+        <ul class="list-disc m-4">
+            <li><a class="hover:underline" target="_blank" href="https://tailwindcss.com/docs/adding-custom-styles">Adding Custom Styles</a></li>
+            <li><a class="hover:underline" target="_blank" href="https://tailwindcss.com/docs/adding-custom-styles#adding-component-classes">Adding component classes</a></li>
+            <li><a class="hover:underline" target="_blank" href="https://github.com/tailwindlabs/tailwindcss-forms">TailwindCSS Forms plugin</a></li>
+            <li><a class="hover:underline" target="_blank" href="https://tailwindcss.com/docs/plugins#css-in-js-syntax">CSS-in-JS syntax</a></li>
+        </ul>
+    </div>
+
+</div>
---
+++ src/routes/form-components/+page.svelte
@@ -0,0 +1,88 @@
+<script lang="ts">
+    const types = [
+        'text',
+        'password',
+        'email',
+        'number',
+        'url',
+        'date',
+        'datetime-local',
+        'month',
+        'week',
+        'time',
+        'search',
+        'tel',
+    ];
+</script>
+
+<div class="m-4">
+    <h1 class="text-2xl">Inputs</h1>
+    <ul class="list-disc m-4">
+        {#each types as type}
+            <li><input {type} placeholder={type} /></li>
+        {/each}
+    </ul>
+
+    <hr class="mt-4 mb-4" />
+
+    <h1 class="text-2xl">checkboxes</h1>
+    <ul>
+        <li><label><input type="checkbox" />one</label></li>
+        <li><label><input type="checkbox" />two</label></li>
+        <li><label><input type="checkbox" />three</label></li>
+    </ul>
+
+    <hr class="mt-4 mb-4" />
+
+    <h1 class="text-2xl">radio group</h1>
+    <ul>
+        <li><label><input type="radio" name="radio-group" />one</label></li>
+        <li><label><input type="radio" name="radio-group" />two</label></li>
+        <li><label><input type="radio" name="radio-group" />three</label></li>
+    </ul>
+
+    <hr class="mt-4 mb-4" />
+
+    <h1 class="text-2xl">buttons</h1>
+    <ul>
+        <li><button type="button">unstyled normal</button></li>
+        <li>
+            <button type="button" class="local-button">locally-styled button</button> (see
+            <code>&lt;style&gt;</code> tag in the code for this page)
+        </li>
+    </ul>
+
+    <hr class="mt-4 mb-4" />
+
+    <h1 class="text-2xl">select</h1>
+    <ul class="pb-8">
+        <li>
+            unstyled <select>
+                <option>one</option>
+                <option>two</option>
+                <option>three</option>
+            </select>
+        </li>
+        <li>
+            <a
+                href="https://tailwindui.com/components/application-ui/forms/select-menus#component-79592f355ca6f6e3b081e0158109981c"
+                class="hover:underline">simple native (from tailwind-ui)</a
+            ><select
+                id="location"
+                name="location"
+                class="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            >
+                <option>United States</option>
+                <option selected>Canada</option>
+                <option>Mexico</option>
+            </select>
+        </li>
+    </ul>
+</div>
+
+<style>
+    .local-button {
+        border: 1px solid black;
+        padding: 5px;
+    }
+</style>
